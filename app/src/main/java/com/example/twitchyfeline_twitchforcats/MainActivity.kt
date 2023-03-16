bcb11fdcd547a3558c946f8a8a9ee518b39e5869
reagandhar <reagandhar@gmail.com>
2023-03-16 17:34:02 -0400

    Initial commit

---
+++ app/src/main/java/com/example/twitchyfeline_twitchforcats/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.twitchyfeline_twitchforcats
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
